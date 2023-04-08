e8777219467f4491987e42b12bbf9bcfdeb93136
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-08 17:57:49 +0200

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
+}
