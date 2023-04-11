9f9a8fc7e6a7e143062373e577e3353e43563a02
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-12 01:32:23 +0200

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_strdup - function that returns a ptr to a newly allocated space in memo
+ * @str: pointer to array of string
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int size = 0, i;
+	char *n_str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	n_str = (char *)malloc(sizeof(char) * (size + 1));
+	if (n_str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < str[size]; i++)
+		{
+			n_str[i] = str[i];
+		}
+	}
+	return (n_str);
+}
