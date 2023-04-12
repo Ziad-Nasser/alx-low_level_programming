81f9bf95949123023e481be90f7945ed054d1154
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-12 03:04:58 +0200

    ziad

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2 = '\0';
 	}
 	size1 = size2 = 0;
 	while (s1[size1] != '\0')
