08e2149e01260f3114d6645829063217ae95e3f7
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-12 17:40:14 +0200

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * argstostr - concatenates all the arguments
+ * @ac: input
+ * @av: 2d pointer array of string
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int size1, size2, tsize = 0, rtsize = 0;
+	char *n_str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (size1 = 0; size1 < ac; size1++)
+	{
+		for (size2 = 0; av[size1][size2]; size2++)
+		{
+			size1++;
+		}
+	}
+	size1 += ac;
+	n_str = (char *)malloc(sizeof(char) * rtsize + 1);
+
+	if (n_str == NULL)
+	{
+		return (NULL);
+	}
+	for (size1 = 0; size1 < ac; size1++)
+	{
+		for (size2 = 0; av[size1][size2]; size2++)
+		{
+			n_str[tsize] = av[size1][size2];
+			tsize++;
+		}
+	}
+
+	if (n_str[tsize] == '\0')
+	{
+		n_str[tsize] = '\n';
+	}
+	return (n_str);
+}
