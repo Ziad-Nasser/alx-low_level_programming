bce7d36b0894bb8d818c9965ab4946ea1824c197
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-12 03:01:40 +0200

    ziad

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,18 +9,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
 	char *n_str;
+	int size1, size2;
 
 	if (s1 == NULL)
 	{
-		s1 = '\0';
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = '\0';
+		s2 = "";
 	}
-
+	size1 = size2 = 0;
 	while (s1[size1] != '\0')
 	{
 		size1++;
@@ -29,22 +29,23 @@ char *str_concat(char *s1, char *s2)
 	{
 		size2++;
 	}
-	n_str = (char *)malloc(sizeof(char) * (size1 + size2) + 1);
+	n_str = malloc(sizeof(char) * (size1 + size2 + 1));
+
 	if (n_str == NULL)
 	{
 		return (NULL);
 	}
-	size1 = 0, size2 = 0;
-	for (i = 0; i <= size1 + size2; i++)
+	size1 = size2 = 0;
+	while (s1[size1] != '\0')
 	{
-		if (size1 > i)
-		{
-			n_str[i] = s1[i];
-		}
-		else
-		{
-			n_str[i] = s2[i - size1];
-		}
+		n_str[size1] = s1[size1];
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		n_str[size1] = s2[size2];
+		size1++, size2++;
 	}
 	n_str[size1] = '\0';
 	return (n_str);
