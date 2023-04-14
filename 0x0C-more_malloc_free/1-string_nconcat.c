6c23e0472ff4658942d5fa24d4daa57e49ced49f
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-14 03:22:48 +0200

    ziad

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,15 +38,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (str == NULL)
 		return (NULL);
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-		str[a] = s1[a];
-	}
-	for (b = 0; b < n; b++)
-	{
-		str[a] = s2[b];
-		a++;
-	}
+	while (n < size2 && a < (size1 + n))
+		str[a++] = s2[b++];
+
+	while (n >= size2 && a < (size1 + size2))
+		str[a++] = s2[b++];
+
 	str[a] = '\0';
 	return (str);
 }
