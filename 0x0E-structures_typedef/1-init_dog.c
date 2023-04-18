2dcfbd880f8763badb2cb263953d0dd1133b3535
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-18 04:01:54 +0200

    the last edit in the task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - initialize a variable of type struct dog
@@ -7,7 +8,6 @@
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,41 +11,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
+	dog_t *dog;
 
-	my_dog = malloc(sizeof(dog_t));
-	if (my_dog == NULL)
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
 
-	my_dog->age = age;
+	dog->age = age;
 
 	if (name != NULL)
 	{
-		my_dog->name = malloc(strlen(name) + 1);
-		if (my_dog->name == NULL)
+		dog->name = malloc(strlen(name) + 1);
+		if (dog->name == NULL)
 		{
-			free(my_dog);
+			free(dog);
 			return (NULL);
 		}
 		else
 		{
-			strcpy(my_dog->name, name);
+			strcpy(dog->name, name);
 		}
 	}
 
 	if (owner != NULL)
 	{
-		my_dog->owner = malloc(strlen(owner) + 1);
-		if (my_dog->owner == NULL)
+		dog->owner = malloc(strlen(owner) + 1);
+		if (dog->owner == NULL)
 		{
-			free(my_dog->name);
-			free(my_dog);
+			free(dog->name);
+			free(dog);
 			return (NULL);
 		}
 		else
 		{
-			strcpy(my_dog->owner, owner);
+			strcpy(dog->owner, owner);
 		}
 	}
-	return (my_dog);
+	return (dog);
 }
