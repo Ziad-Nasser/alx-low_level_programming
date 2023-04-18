03cafb781d74f770e995a23189f050e6299d62a6
Ziad-Nasser <ziadnasser177@gmail.com>
2023-04-18 02:02:51 +0200

    structure 1&4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - to know information about the dog 
+ * struct dog - to know information about the dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
