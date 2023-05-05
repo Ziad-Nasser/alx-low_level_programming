337ee324873f21b11bd333b7c1920b29a5861f31
Ziad-Nasser <ziadnasser177@gmail.com>
2023-05-05 05:09:15 +0300

    done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * set_bit - returns the value of a bit at a given index
+ * clear_bit - returns the value of a bit at a given index
  * @n: number
  * @index: number
  * Return: 1 or -1 if an error occured
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1 << index;
 
