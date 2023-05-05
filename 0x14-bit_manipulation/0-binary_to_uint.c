e8bc45897deb52d31cc594fc24f64605056ed9f3
Ziad-Nasser <ziadnasser177@gmail.com>
2023-05-05 03:06:11 +0300

    finallygit add .

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+		{
+			num = (num << 1);
+		}
+		else if (b[i] == '1')
+		{
+			num = ((num << 1) | 1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (num);
+}
